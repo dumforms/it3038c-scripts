062a0c6f9e081a2f34a30de3951376e5af5cd770
dumforms <84052826+dumforms@users.noreply.github.com>
2023-01-13 19:51:39 -0500

    Update myFirstScript.sh

--- bash/myFirstScript.sh
+++ bash/myFirstScript.sh
@@ -12,5 +12,6 @@ echo Working Dir: $PWD
 echo Session Length: $SECONDS
 echo Home Dir: $HOME
 
+# I did this on an Ubuntu machine instead of the Sandbox CentOS machine, so my command looks a little different than the lab
 a=$(ip a | grep 'global ens18' | awk '{print $2}')
 echo My IP is $a
