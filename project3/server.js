d8523cd23de9ef96c1cd72a4e3ff4f4062870c5c
dumforms <84052826+dumforms@users.noreply.github.com>
2023-04-11 23:34:35 -0400

    funky commit

---
+++ project3/server.js
@@ -0,0 +1,29 @@
+// Check for dev environment & load dev-only modules
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+// Require modules
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+// Require route files
+const indexRouter = require('./routes/index')
+
+// Set view and view engine
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')  // Server views here
+app.set('layout', 'layouts/layout')     // HTML layout templates here
+app.use(expressLayouts)
+app.use(express.static('public'))       // Public views here
+
+app.use('/', indexRouter)
+
+// Set listening port
+app.listen(process.env.PORT || 3000)
